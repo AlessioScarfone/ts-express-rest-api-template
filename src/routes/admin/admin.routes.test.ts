d51d09423e9ab64e18b093e87d28b033657fc1e9
Alessio Scarfone <alessioscarfone93@gmail.com>
2022-07-26 09:29:18 +0200

    feat: admin health check test

--- src/routes/admin/admin.routes.test.ts
+++ src/routes/admin/admin.routes.test.ts
@@ -2,7 +2,6 @@ import 'jest';
 import * as express from 'express';
 import request from 'supertest';
 import BootstrapHelpers from '../../tests/helper/integration-helpers';
-import { STATUS_CODES } from 'http';
 
 describe('admin healt tests', () => {
     let app: express.Application;
@@ -12,7 +11,6 @@ describe('admin healt tests', () => {
 
     it('can get server health', async () => {
         const response = await request(app).get('/admin/health');
-        console.log(">>", response.statusCode)
         expect(response.statusCode).toBe(200);
         expect(response.body.message).toBe("ok");
     });
