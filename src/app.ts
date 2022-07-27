c59150cc34490e09fd021fb9307bb28c245bd0de
Alessio Scarfone <alessioscarfone93@gmail.com>
2022-07-27 08:35:17 +0200

    feat: update sample env

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,8 @@ import loadHelmetMiddleware from "./loaders/loadHelmetMiddleware";
 const loadExpressMiddleware = (app: Express) => {
   if (env.monitor.enabled && env.monitor.user && env.monitor.page && env.monitor.password)
     loadMonitorPage(app)
+  else 
+    console.log(">> Monitor not configured <<")
 
   loadHelmetMiddleware(app);
   app.use(cors());
