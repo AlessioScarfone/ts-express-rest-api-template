d6a937fe882ed498323d5d94072740b106c6660a
Alessio Scarfone <alessioscarfone93@gmail.com>
2022-07-29 09:09:59 +0200

    update unit test

--- src/routes/admin/admin.routes.test.ts
+++ src/routes/admin/admin.routes.test.ts
@@ -11,7 +11,7 @@ describe('Admin Router tests', () => {
     });
 
     it('can get server health', async () => {
-        const response = await request(app).get('/admin/health');
+        const response = await request(app).get('/api/admin/health');
         expect(response.statusCode).toBe(200);
         expect(response.body.message).toBe("ok");
     });
